d0e71ba8451c1e677793182c414987675fcf5797
Administrator <Administrator@User-2021ZLTIPQ>
2022-01-11 15:50:39 +0900

    220111 이택진

--- yologaza/src/main/java/com/myspring/yologaza/reservation/controller/ReservationControllerImpl.java
+++ yologaza/src/main/java/com/myspring/yologaza/reservation/controller/ReservationControllerImpl.java
@@ -1,5 +1,80 @@
 package com.myspring.yologaza.reservation.controller;
 
-public class ReservationControllerImpl implements ReservationController {
-	 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.myspring.yologaza.common.base.BaseController;
+import com.myspring.yologaza.goods.service.GoodsService;
+import com.myspring.yologaza.goods.vo.GoodsVO;
+import com.myspring.yologaza.member.vo.MemberVO;
+import com.myspring.yologaza.sms.service.certificationService;
+
+@Controller("ReservationController")
+@RequestMapping(value="/reservation")
+public class ReservationControllerImpl extends BaseController implements ReservationController {
+	@Autowired
+	private MemberVO memberVO;
+	@Autowired
+	private GoodsService goodsService;
+	
+	@RequestMapping(value="/reservationForm.do", method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView reservationForm(@RequestParam("goods_id") String goods_id, 
+								HttpServletRequest request, 
+								HttpServletResponse response) throws Exception {
+		//String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
+		HttpSession session = request.getSession();
+		ModelAndView mav = new ModelAndView();
+		Map goodsMap=goodsService.goodsDetail(goods_id);
+		mav.addObject("goodsMap", goodsMap);
+		GoodsVO goodsVO=(GoodsVO)goodsMap.get("goodsVO");
+		mav.setViewName(viewName);
+		
+		long today = (System.currentTimeMillis()/1000) + 32400;
+		Date date = new Date(System.currentTimeMillis()+32400000);
+		SimpleDateFormat timeFormat = new SimpleDateFormat("MM/dd/yyyy");
+		String todayDate = timeFormat.format(date);
+		long date1 = 0;
+		long date2 = 0;
+		if(request.getParameter("date1") != null)
+			date1 = Long.parseLong(request.getParameter("date1"));
+		if(request.getParameter("date2") != null)
+			date2 = Long.parseLong(request.getParameter("date2"));
+		date1 = (date1/86400) * 86400;
+		date2 = ((date2/86400) * 86400)+1;
+		request.setAttribute("date1", date1);
+		request.setAttribute("date2", date2);
+		String Ddate1 = todayDate;
+		String Ddate2 = todayDate;
+		if(date1 != 0 && date2 != 0) {
+			Ddate1 = timeFormat.format(date1*1000);
+			Ddate2 = timeFormat.format(date2*1000);
+		}
+		request.setAttribute("Ddate1", Ddate1);
+		request.setAttribute("Ddate2", Ddate2);
+		return mav;
+	}
+	
+	@Override
+	@RequestMapping(value = "/phoneCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public String sendSMS(@RequestParam("phone") String userPhoneNumber) {
+		int randomNumber = (int)((Math.random()* (9999 - 1000 + 1)) + 1000);
+		certificationService.certifiedPhoneNumber(userPhoneNumber,randomNumber);
+		return Integer.toString(randomNumber);
+	}
 }
